360352c79ca6e211adbd396396cc148604ecb618
Creator125OCA <85368990+Creator125@users.noreply.github.com>
2021-06-17 20:12:55 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,32 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    double a=0;
+    double b=0;
+    double c=0;
+    Console.WriteLine ("Suma");
+
+    //La variable c sera igual al metodo suma
+    c= Suma();
+
+//Imprimiendo el valor c cuyo resultado sera la retonación del metodo suma
+    Console.WriteLine("El resultado del metodo suma es: " + c);
+
+  }
+  
+public static double Suma(){//Llave que abre el metodo suma
+ /* //Estableciendo las variables
+  int var1 = 5;
+  int var2 = 6;
+ //La variable que sumará las dos variable anteriores
+  int resul = var1 + var2;
+
+//Retomando a la variable resul
+  return resul;
+}//Llave que cierra el metodo  suma
+*/
+
+
+}
